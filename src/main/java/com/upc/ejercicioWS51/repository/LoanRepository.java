715fd335f7db90f8e5191d4b90f57bd9f1c81670
Luis D. Maco <luisdmaco@outlook.com>
2023-05-30 21:31:41 -0500

    LoanRepository fixed and connection with db ready

--- src/main/java/com/upc/ejercicioWS51/repository/LoanRepository.java
+++ src/main/java/com/upc/ejercicioWS51/repository/LoanRepository.java
@@ -1,12 +1,13 @@
 package com.upc.ejercicioWS51.repository;
 
+import com.upc.ejercicioWS51.model.Book;
 import com.upc.ejercicioWS51.model.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface LoanRepository extends JpaRepository<Loan, Long> {
+    boolean existsByCodeStudentAndBookAndBookLoan(String codeStudent, Book book, boolean bookLoan);
     boolean existsByCodeStudent(String codeStudent);
-    boolean existsByCodeStudentAndBook(String codeStudent, Long bookId, boolean bookLoan);
     List<Loan> findByCodeStudent(String codeStudent);
 }
