2bf73ea6a91b4d510b3756b71e6e8d2cab2e5d92
Luis D. Maco <luisdmaco@outlook.com>
2023-06-02 20:52:42 -0500

    LoanController fixed

--- src/main/java/com/upc/ejercicioWS51/controller/LoanController.java
+++ src/main/java/com/upc/ejercicioWS51/controller/LoanController.java
@@ -1,19 +1,16 @@
 package com.upc.ejercicioWS51.controller;
 
+
+import com.upc.ejercicioWS51.exception.ResourceNotFoundException;
 import com.upc.ejercicioWS51.exception.ValidationException;
 import com.upc.ejercicioWS51.model.Book;
 import com.upc.ejercicioWS51.model.Loan;
-import com.upc.ejercicioWS51.repository.BookRepository;
 import com.upc.ejercicioWS51.repository.LoanRepository;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.ResourceAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.upc.ejercicioWS51.repository.BookRepository;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -21,6 +18,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/library/v1")
 public class LoanController {
+
     private LoanRepository loanRepository;
     private BookRepository bookRepository;
 
@@ -29,44 +27,43 @@ public class LoanController {
         this.bookRepository = bookRepository;
     }
 
-    //EndPoint: http://localhost:8080/api/library/v1/loans/filterByCodeStudent
+    //EndPoint: http://lovalhost:8080/api/library/v1/loans/filterByCodeStudent
     //Method: GET
     @Transactional(readOnly = true)
     @RequestMapping("/loans/filterByCodeStudent")
-    public ResponseEntity<List<Loan>> getLoansByCodeStudent(@RequestParam(name="codeStudent") String codeStudent){
-        return new ResponseEntity<List<Loan>>(loanRepository.findByCodeStudent(codeStudent), HttpStatus.OK);
+    public ResponseEntity<List<Loan>> getLoansByCodeStudent(@RequestParam String codeStudent) {
+        List<Loan> loans = loanRepository.findByCodeStudent(codeStudent);
+        return ResponseEntity.ok(loans);
     }
 
-    //EndPoint: http://localhost:8080/api/library/v1/books/1/loans
-    //Method: GET
-    @Transactional(readOnly = true)
-    @RequestMapping("books/{id}/loans")
-    public ResponseEntity<Loan> createLoan(@RequestParam(name = "id") long bookId,
+    //EndPoint: http://lovalhost:8080/api/library/v1/books/1/loans
+    //Method: Post
+    @Transactional
+    @RequestMapping("/books/{id}/loans")
+    public ResponseEntity<Loan> createLoan(@PathVariable(value = "id") long bookId,
                                            @RequestBody Loan loan) {
         Book book = bookRepository.findById(bookId)
-                .orElseThrow(() -> new ResourceAccessException("No se encontro el libro con id: " + bookId));
+                .orElseThrow(() -> new ResourceNotFoundException("No se encuentra el libro con el id: " + bookId));
         existsByCodeStudentAndBookAndBookLoan(loan, book);
         validateLoan(loan);
-        loan.setBook(book);
         loan.setLoanDate(LocalDate.now());
+        loan.setDevolutionDate(LocalDate.now().plusDays(3));
         loan.setBookLoan(true);
-        return new ResponseEntity<>(loanRepository.save(loan), HttpStatus.CREATED);
+        return new ResponseEntity<Loan>(loanRepository.save(loan), HttpStatus.CREATED);
     }
 
     private void validateLoan(Loan loan) {
-        if (loan.getCodeStudent() == null || loan.getCodeStudent().trim().isEmpty()) {
-            throw new ValidationException("El codigo del estudiante debe ser obligatorio.");
+        if(loan.getCodeStudent() == null || loan.getCodeStudent().isEmpty()) {
+            throw new RuntimeException("El código de estudiante es obligatorio");
         }
-        if (loan.getCodeStudent().length() < 10) {
-            throw new ValidationException("El codigo del estudiante no debe exceder los 10 caracteres.");
+        if(loan.getCodeStudent().length() > 10) {
+            throw new ValidationException("El código de estudiante no puede tener 10 caracteres");
         }
     }
 
-    private void existsByCodeStudentAndBookAndBookLoan(Loan loan, Book book) {
-        if (loanRepository.existsByCodeStudentAndBookAndBookLoan(loan.getCodeStudent(), book, true)) {
-            throw new ValidationException("El prestamo del libro "+ book.getTitle() +
-                    " no existe porque ya fue prestado por el alumno "+ loan.getCodeStudent()
-            );
+    private void existsByCodeStudentAndBookAndBookLoan(Loan loan, Book book){
+        if(loanRepository.existsByCodeStudentAndBookAndBookLoan(loan.getCodeStudent(), book, true)){
+            throw new ValidationException("El prestamo del libro " + book.getTitle() + " no es posible porque ya fue prestado por el alumno"+ loan.getCodeStudent());
         }
     }
 }
